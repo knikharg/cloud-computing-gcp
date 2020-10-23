e4ea503baad80c407bbe2bfe9929fc1573a680ef
knikharg <knikharg@iu.edu>
2020-10-23 16:15:01 -0400

    port changes

--- keyvalue-startup.sh
+++ keyvalue-startup.sh
@@ -10,4 +10,8 @@ rm -rf map-reduce-gcp
 git clone https://github.com/knikharg/map-reduce-gcp.git
 cd map-reduce-gcp/
 git pull
-java -jar keyvaluestore-0.0.1-SNAPSHOT-jar-with-dependencies.jar
+
+kvPort=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/kvPort -H "Metadata-Flavor: Google")
+echo kvPort -$kvPort
+
+java -jar keyvaluestore-0.0.1-SNAPSHOT-jar-with-dependencies.jar $kvPort
--- master-startup.sh
+++ master-startup.sh
@@ -11,6 +11,12 @@ rm -rf map-reduce-gcp
 git clone https://github.com/knikharg/map-reduce-gcp.git
 cd map-reduce-gcp/
 git pull
-java -jar mapreduce-0.0.1-SNAPSHOT-jar-with-dependencies.jar
+
+kvPort=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/kvPort -H "Metadata-Flavor: Google")
+masterPort=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/masterPort -H "Metadata-Flavor: Google")
+
+echo kvPort -$kvPort,masterPort-$masterPort
+
+java -jar mapreduce-0.0.1-SNAPSHOT-jar-with-dependencies.jar $kvPort $masterPort
 
 
